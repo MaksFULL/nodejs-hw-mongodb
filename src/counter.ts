2898e8d399a57c27bce27c279d1a8ed7a4cda890
MaksFULL <169079873+MaksFULL@users.noreply.github.com>
2025-03-12 00:48:05 +0200

    fix

--- src/counter.ts
+++
@@ -1,9 +0,0 @@
-export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0;
-  const setCounter = (count: number) => {
-    counter = count;
-    element.innerHTML = `count is overkill ${counter}`;
-  };
-  element.addEventListener("click", () => setCounter(counter + 1));
-  setCounter(100500);
-}
